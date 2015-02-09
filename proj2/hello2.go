a9f3fc34e34537496984fc242abad4ebc2254807
Puneet Sharma <psharm03@users.noreply.github.com>
2015-02-09 11:35:03 -0500

    first
    adding first files.

---
+++ proj2/hello2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+type Vertex struct {
+  X, Y int
+}
+
+var (
+  v1 = Vertex{1, 2}  // has type Vertex
+  v2 = Vertex{X: 1}  // Y:0 is implicit
+  v3 = Vertex{}      // X:0 and Y:0
+  p  = &Vertex{1, 2} // has type *Vertex
+)
+
+func say(s string) {
+  for i := 0; i < 5; i++ {
+    time.Sleep(100 * time.Millisecond)
+    fmt.Println(s)
+  }
+}
+
+func main() {
+  var a [2]string
+  a[0] = "Hello"
+  a[1] = "World"
+  fmt.Println(a[0], a[1])
+  fmt.Println(a)
+
+  p := []int{2, 3, 5, 7, 11, 13}
+  fmt.Println("p ==", p)
+
+  for i := 0; i < len(p); i++ {
+    fmt.Printf("p[%d] == %d\n", i, p[i])
+  }
+
+  v := Vertex{1, 2}
+  p := &v
+  p.X = 1e9
+  fmt.Println(v)
+
+  fmt.Println(v1, p, v2, v3)
+
+  go say("world")
+  say("hello")
+
+}
